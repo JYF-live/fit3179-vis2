9db3a777d48e1ca13e6b0f88ce1102e3798792e2
Julian Foote <jfoo0022@student.monash.edu>
2025-10-12 14:00:28 +1100

    added the pie charts as well as .js file and edited radial charts html to test filter

--- radial_charts.js
+++ radial_charts.js
@@ -1,14 +1,16 @@
 // Store the embedded views for later updates
-let tempView, gustView, rainView;
+let tempView, gustView, rainView, tempPieView, rainPieView;
 
 // Load the chart specifications from JSON files
 Promise.all([
     fetch('temperature_radial.json').then(response => response.json()),
     fetch('wind_gust_radial.json').then(response => response.json()),
-    fetch('rainfall_radial.json').then(response => response.json())
-]).then(function([tempSpec, gustSpec, rainSpec]) {
+    fetch('rainfall_radial.json').then(response => response.json()),
+    fetch('gusty_days_temperature_pie.json').then(response => response.json()),
+    fetch('gusty_days_rainfall_pie.json').then(response => response.json())
+]).then(function([tempSpec, gustSpec, rainSpec, tempPieSpec, rainPieSpec]) {
     
-    // Embed the charts and store the views
+    // Embed the radial charts and store the views
     vegaEmbed('#tempChart', tempSpec, {actions: false}).then(result => {
         tempView = result.view;
     });
@@ -18,12 +20,20 @@ Promise.all([
     vegaEmbed('#rainChart', rainSpec, {actions: false}).then(result => {
         rainView = result.view;
     });
+    
+    // Embed the pie charts and store the views
+    vegaEmbed('#tempPieChart', tempPieSpec, {actions: false}).then(result => {
+        tempPieView = result.view;
+    });
+    vegaEmbed('#rainPieChart', rainPieSpec, {actions: false}).then(result => {
+        rainPieView = result.view;
+    });
 
     // Handle state selection change
     document.getElementById('stateSelect').addEventListener('change', function(e) {
         const selectedState = e.target.value;
         
-        // Update the parameter value in each view
+        // Update the parameter value in each radial chart view
         if (tempView) {
             tempView.signal('stateSelection', selectedState).run();
         }
@@ -32,6 +42,14 @@ Promise.all([
         }
         if (rainView) {
             rainView.signal('stateSelection', selectedState).run();
+        }
+        
+        // Update the parameter value in each pie chart view
+        if (tempPieView) {
+            tempPieView.signal('stateSelection', selectedState).run();
+        }
+        if (rainPieView) {
+            rainPieView.signal('stateSelection', selectedState).run();
         }
     });
 }).catch(function(error) {
